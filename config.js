d79cace87aca93b0a0564d236ef7757ff460ab9a
Carine <carinengbeechin@gmil.com>
2019-12-24 18:46:30 +0800

    'final'

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+		host: 'localhost',
+		port: 3306,
+		user: 'fred', password: '181204Qw!',
+		database: 'northwind',                                          //change
+		connectionLimit: 4,
+		sessionSecret: 'my secret'
+}
+
+// rmb to create in local instead, then grant access to fred
